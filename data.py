0b777fcdd0e4e47f713c6468b6fa78ad123b6822
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-21 15:10:32 +0530

    newchange

--- data.py
+++ data.py
@@ -41,3 +41,10 @@ my_string = 'Hello'
 print(my_string)
 my_string = "Hello"
 print(my_string)
+my_string = '''Hello'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
