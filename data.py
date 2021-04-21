ce1cefcd59a62422e0efaf4f64be6d68f38435db
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-21 15:07:33 +0530

    new

---
+++ data.py
@@ -0,0 +1,37 @@
+# List indexing
+
+my_list = ['p', 'r', 'o', 'b', 'e']
+
+# Output: p
+print(my_list[0])
+
+# Output: o
+print(my_list[2])
+
+# Output: e
+print(my_list[4])
+
+# Nested List
+n_list = ["Happy", [2, 0, 1, 5]]
+
+# Nested indexing
+print(n_list[0][1])
+
+print(n_list[1][3])
+
+# Error! Only integer can be used for indexing
+print(my_list[4])
+my_tuple = ()
+print(my_tuple)
+
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# tuple with mixed datatypes
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+
+# nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
