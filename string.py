7681e307b4f06cfe2c8029131e17b769db66e0c6
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-22 11:33:20 +0530

    newchange

--- string.py
+++ string.py
@@ -15,3 +15,14 @@ def appendMiddle(s1, s2):
   print("After appending new string in middle", middleThree)
 
 appendMiddle("Ault", "Kelly")
+def mix_string(s1, s2):
+    first_char = s1[:1] + s2[:1]
+    middle_char = s1[int(len(s1) / 2):int(len(s1) / 2) + 1] + s2[int(len(s2) / 2):int(len(s2) / 2) + 1]
+    last_char = s1[len(s1) - 1] + s2[len(s2) - 1]
+    res = first_char + middle_char + last_char
+    print("Mix String is ", res)
+
+s1 = "America"
+s2 = "Japan"
+mix_string(s1, s2)
+
