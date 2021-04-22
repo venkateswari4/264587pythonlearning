73ae640f81a58578ef931338721900b74010a739
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-22 11:32:20 +0530

    newchange

--- string.py
+++ string.py
@@ -7,3 +7,11 @@ def getMiddleThreeChars(sampleStr):
 
 getMiddleThreeChars("JhonDipPeta")
 getMiddleThreeChars("Jasonay")
+
+def appendMiddle(s1, s2):
+  middleIndex = int(len(s1) /2)
+  print("Original Strings are", s1, s2)
+  middleThree = s1[:middleIndex:]+ s2 +s1[middleIndex:]
+  print("After appending new string in middle", middleThree)
+
+appendMiddle("Ault", "Kelly")
