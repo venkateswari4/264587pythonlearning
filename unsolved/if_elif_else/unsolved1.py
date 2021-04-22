63a5591be9b2d1d45bce034e7ee501a7c31de860
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-22 20:41:16 +0530

    newchange

---
+++ unsolved/if_elif_else/unsolved1.py
@@ -0,0 +1,9 @@
+i=input()
+if i=="lol":
+    print("laughing out loud")
+elif i=="rofl":
+    print("rolling on the floor")
+elif i=="lmk":
+    print("let me know")
+elif i=="smh":
+    print("shaking my head")
