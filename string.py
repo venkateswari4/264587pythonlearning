4c04b90e8580af526e9fe851059d461162a0fec8
venkateswari4444 <seedella.venkateswari@gmail.com>
2021-04-22 11:30:36 +0530

    newchange

--- string.py
+++ string.py
@@ -2,7 +2,7 @@ def getMiddleThreeChars(sampleStr):
     middleIndex = int(len(sampleStr) / 2)
     print("Original String is", sampleStr)
     middleThree = sampleStr[middleIndex - 1:middleIndex + 2]
-    print("Middle three chars are", middleThree)
+    print("Middle three chars ", middleThree)
 
 
 getMiddleThreeChars("JhonDipPeta")
